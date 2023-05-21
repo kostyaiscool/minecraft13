42f44721d90dc4827c1e5b854b6ac8adc7ce6954
Volodymyr Shargorodskyi <gsmkibernetik@gmail.com>
2023-05-21 12:16:07 +0300

    create minecraft init

---
+++ main.py
@@ -0,0 +1,25 @@
+from direct.showbase.ShowBase import ShowBase
+from map import Map
+from panda3d.core import loadPrcFileData
+# game = ShowBase()
+
+configVars = '''
+win-size 2300 1000
+show-frame-rate-meter 1
+'''
+loadPrcFileData('', configVars)
+
+
+class MyGame(ShowBase):
+    def __init__(self):
+        super().__init__(self)
+        self.set_background_color((0.1, 0.191, 0.3, 0.5))
+        # self.model1 = loader.loadModel('models/environment')
+        # self.model1.reparentTo(render)
+        self.land = Map()
+
+
+game = MyGame()
+
+game.run()
+
---
+++ map.py
@@ -0,0 +1,19 @@
+class Map:
+    def __init__(self):
+        self.default_image = 'source/derevo.jpg'
+        self.default_model = 'source/block.egg'
+        self.create_branch()
+        for x in range (10):
+            for y in range (10, 20):
+                for z in range(-5, -3):
+                    self.create_block((x, y, z))
+
+    def create_block(self, position):
+        self.model = loader.loadModel(self.default_model)
+        self.texture = loader.loadTexture(self.default_image)
+        self.model.setTexture(self.texture)
+        self.model.setPos(position)
+        self.model.reparentTo(self.branch)
+
+    def create_branch(self):
+        self.branch = render.attachNewNode('map_branch')
